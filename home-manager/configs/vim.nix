67650ad5a5ed64e59af29d5f496d8a40d8c16560
p4ncake <me@p4ncake.fr>
2025-01-28 16:03:39 +0100

    update vim config

--- home-manager/configs/vim.nix
+++ home-manager/configs/vim.nix
@@ -7,13 +7,16 @@
       vim-airline
       nerdtree
       telescope-nvim
-      symbols-outline-nvim
+			symbols-outline-nvim
+			telescope-fzf-native-nvim
     ];
 
     extraConfig = ''
 			set tabstop=2
 			set shiftwidth=2
 			set number
+			map <C-n> :NERDTreeToggle<CR>
+			map <C-m> :Telescope<CR>
 		'';
   };
 }
