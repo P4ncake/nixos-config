a2cc43025158508d045bb92272f2acdeea50aed5
p4ncake <me@p4ncake.fr>
2025-02-18 00:35:07 +0100

    upgrade to 24.11

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
   description = "Installation Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
